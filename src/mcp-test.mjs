5a0830054b40b8d065d1239f2ece1d1d37fe75cc
Your Name <you@example.com>
2026-06-09 17:52:44 +0800

    Add mcp-test.mjs

---
+++ src/mcp-test.mjs
@@ -0,0 +1,18 @@
+import "dotenv/config";
+import { MultiServerMCPClient } from "@langchain/mcp-adapters";
+import { ChatOpenAI } from "@langchain/openai";
+import chalk from "chalk";
+import {
+  HumanMessage,
+  SystemMessage,
+  ToolMessage,
+} from "@langchain/core/messages";
+
+const model = new ChatOpenAI({
+  modelName: process.env.MODEL_NAME,
+  apiKey: process.env.OPENAI_API_KEY,
+  configuration: {
+    baseURL: process.env.OPENAI_BASE_URL,
+  },
+});
+
